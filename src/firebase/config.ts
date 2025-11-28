a650544519d7292d530cba0324a2606bde0923d2
Fernando Joana António <infortecmov@gmail.com>
2025-11-28 21:45:15 +0000

    Application error: a client-side exception has occurred while loading 90

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDeRS_6-Ld-AGRwBuUTtY_0uPODyR2GXGs",
+  authDomain: "studio-6625912079-68c87.firebaseapp.com",
+  projectId: "studio-6625912079-68c87",
+  storageBucket: "studio-6625912079-68c87.appspot.com",
+  messagingSenderId: "955544097624",
+  appId: "1:955544097624:web:30983ef390b71850070b2d",
+  measurementId: "G-7Z06DC2P92",
 };

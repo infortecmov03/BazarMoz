a6070c7b3f1aabae58c7cf1c4552ada0bcb4117f
Fernando Joana António <infortecmov@gmail.com>
2025-11-28 15:00:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4196856007-f07d7",
+  "appId": "1:877614046284:web:af1cb7083eed1772fe68e9",
+  "apiKey": "AIzaSyB29TmtBjP5KJOfzX_8neHj45kitLlI9Tc",
+  "authDomain": "studio-4196856007-f07d7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "877614046284"
+};
